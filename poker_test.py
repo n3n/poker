f5dd08f4481cfd1d0ccfe87700769d8f5c1c61c7
n3n <s6070069@kmitl.ac.th>
2013-10-05 12:19:52 +0700

    update source code

--- poker_test.py
+++ poker_test.py
@@ -32,24 +32,31 @@ class TestPoker(unittest.TestCase):
 		expected = True
 		self.assertEqual(actual, expected)
 		
-	def full_house(self):
-		'''Test full_house with ['5C', 'TC', '9C', '3C', '7C']'''
+	def full_house_F(self):
+		'''Test full_house (False) with ['5C', 'TC', '9C', '3C', '7C']'''
 		
 		actual = poker.full_house(['5C', 'TC', '9C', '3C', '7C'])
+		expected = False
+		self.assertEqual(actual, expected)
+	
+	def full_house_T(self):
+		'''Test full_house (True) with ['5C', '5S', '5H', '3C', '3H']'''
+		
+		actual = poker.full_house(['5C', '5S', '5H', '3C', '3H'])
 		expected = True
 		self.assertEqual(actual, expected)
 		
 	def kind(self):
-		'''Test 3 of kind with ['5C', 'TC', '9C', '3C', '7C']'''
+		'''Test 3 of kind with ['4C', '4S', '4H', '3C', '7C']'''
 		
-		actual = poker.kind(['5C', 'TC', '9C', '3C', '7C'])
+		actual = poker.kind(['4C', '4S', '4H', '3C', '7C'])
 		expected = True
 		self.assertEqual(actual, expected)
 		
 	def two_pair(self):
-		'''Test twopair with ['5C', 'TC', '9C', '3C', '7C']'''
+		'''Test twopair with ['5C', 'AS', '2C', '3C', '5H']'''
 		
-		actual = poker.twopair(['5C', 'TC', '9C', '3C', '7C'])
+		actual = poker.twopair(['5C', 'AS', '2C', '3C', '5H'])
 		expected = True
 		self.assertEqual(actual, expected)
 	
