d8a5685b5e2579f89696a1a1e17b7587e10f3b6f
n3n <s6070069@kmitl.ac.th>
2013-10-05 11:51:21 +0700

    Add test case

--- poker_test.py
+++ poker_test.py
@@ -4,14 +4,33 @@ import poker
 class TestPoker(unittest.TestCase):
 	'''Example unittest test methods for poker.'''
  
-	def test_poker_straight_flush(self):
+ 	def test_poker(self):
+		'''Test poker with [['JC', 'TC', '9C', '8C', '7C'],['JS', 'TS', '9S', '8S', '7S']]'''
+		
+		actual = poker.poker([['JC', 'TC', '9C', '8C', '7C'],['JS', 'TS', '9S', '8S', '7S']])
+		expected = [['JC', 'TC', '9C', '8C', '7C'], ['JS', 'TS', '9S', '8S', '7S']]
+		self.assertEqual(actual, expected)
+		
+	def test_straight_flush(self):
 		'''Test straight flush with ['JC', 'TC', '9C', '8C', '7C']'''
 		
 		actual = poker.straight_flush(['JC', 'TC', '9C', '8C', '7C'])
 		expected = True
 		self.assertEqual(actual, expected)
+
+	def test_straight(self):
+		'''Test straight flush with ['5S', '5H', '5D', '5C', 'KS']'''
 		
+		actual = poker.straight(['5S', '5H', '5D', '5C', 'KS'])
+		expected = False
+		self.assertEqual(actual, expected)
+		
+	def test_flush(self):
+		'''Test straight flush with ['5C', 'TC', '9C', '3C', '7C']'''
+		
+		actual = poker.flush(['5C', 'TC', '9C', '3C', '7C'])
+		expected = True
+		self.assertEqual(actual, expected)
+	
 if __name__ == '__main__':
 	unittest.main(exit=False)
-
-
