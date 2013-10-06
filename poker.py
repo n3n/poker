fa5e96cb75e4ee92a12c7459c0ed12c23f2fea39
n3n <s6070069@kmitl.ac.th>
2013-10-06 15:34:52 +0700

    add new function, add source in function game

--- poker.py
+++ poker.py
@@ -1,3 +1,6 @@
+import os
+import random
+
 def poker(hands):
     """
    ([hand, hand, ...])-> hands
@@ -17,6 +20,9 @@ def poker(hands):
     return allmax(hands)
  
 def gen_Card():
+	"""
+	generate 52 cards
+	"""
 	deck = []
 	for rank in '23456789TJQKA':
 		for suit in 'CDHS':
@@ -46,7 +52,7 @@ def hand_rank(hand):
         return 8, max(ranks)
     elif kind(4, ranks):
         return 7, kind(4, ranks)
-    elif fullhouse(ranks):
+    elif full_house(ranks):
         return 6, kind(3, ranks), kind(2, ranks)
     elif flush(hand):
         return 5, ranks
@@ -123,13 +129,10 @@ def full_house(ranks):
    false otherwise
  
    >>> sf_ranks = [11, 10, 9, 8, 7]
-   >>> fullhouse(sf_ranks)
-   False
-   >>> fk_ranks = [5, 5, 5, 5, 13]
-   >>> fullhouse(fk_ranks)
+   >>> full_house(sf_ranks)
    False
    >>> fh_ranks = [5, 5, 5, 8, 8]
-   >>> fullhouse(fh_ranks)
+   >>> full_house(fh_ranks)
    True
    """
    return True if kind(3, ranks) and kind(2, ranks) else False
@@ -147,12 +150,6 @@ def kind(n, ranks):
    >>> fk_ranks = [5, 5, 5, 5, 13]
    >>> kind(4, fk_ranks)
    5
-   >>> fh_ranks = [5, 5, 5, 8, 8]
-   >>> kind(3, fh_ranks)
-   5
-   >>> op_ranks = [5, 3, 9, 8, 8]
-   >>> kind(2, op_ranks)
-   8
    """
    
     for r in ranks:
@@ -160,11 +157,53 @@ def kind(n, ranks):
             return r
     return 0
 	
-def twopair(hand):
+def twopair(ranks):
+    """
+   (ranks)-> tuple
+ 
+   Return tuple of highpair and lowpair if hand is twopair,
+   false otherwise
+ 
+   >>> sf_ranks = [11, 10, 9, 8, 7]
+   >>> twopair(sf_ranks)
+   ()
+   >>> tp_ranks = [5, 5, 9, 8, 8]
+   >>> twopair(tp_ranks)
+   (8, 5)
+   """
+    ranks.sort(reverse=True)
+    high_pair = kind(2, ranks)
+    ranks.sort()
+    low_pair = kind(2, ranks)
+    ranks.sort(reverse=True)
+    if high_pair != low_pair:
+        return (high_pair, low_pair)
+    return ()
+
+def singleplayer():
 	pass
 	
+def multiplayer():
+	pass
+		
 def game():
-	pass
 	
-#print flush(['JC', 'TC', '9C', '8C', '7C'])
-#print kind(3, [4, 4, 4, 3, 7])
+	print "|------------------------------------------------------------|"
+	print "|                         Poker Game                         |"
+	print "|------------------------------------------------------------|"
+	
+	print "Select Mode: 1. SinglePlayer"
+	print "             2. Multiplayer"
+	mode = raw_input("Mode: ")
+	if mode == '1':
+		singleplayer()
+	elif mode == '2':
+		player = raw_input("enter player(2-4): ")
+	else:
+		os.system('clear')
+		return "Exit"
+
+#print twopair([11, 10, 9, 8, 7])
+print game()
+#print poker([['JC', 'TC', '9C', '8C', '7C'],['5S', '5H', '5D', '5C', 'KS']])
+#print poker([['9S', '8C', 'KC', '4S', '9H'], ['8D', '3H', 'QS', '3C', 'JD']])
